fe7693835ad037f0ad9fdbf29478b4210a1fb215
utkarshmall13 <utkarshmall13@gmail.com>
2017-02-11 00:00:56 +0530

    slight changes in part 1

--- assign2/1/code/myIntegration.m
+++ assign2/1/code/myIntegration.m
@@ -1,6 +1,6 @@
 function [integral] = myIntegration(image,t,theta,deltas)
 %MYINTEGRATION Integrates on image along the line parametrized t, theta
-theta=degtorad(theta);
+theta=deg2rad(theta);
 
 [dimx,dimy] = size(image);
 center = [t*cos(theta)+dimx/2+0.5,t*sin(theta)+dimy/2+0.5];
@@ -8,7 +8,7 @@ integral=0;
 
 point=center;
 while point(1)<=dimx && point(2)<=dimy && point(1)>=1 && point(2)>=1
-    integral = integral + interp2(image,point(1),point(2));
+    integral = integral + interp2(image,point(1),point(2))*deltas;
     point(1) = point(1)+deltas*sin(theta);
     point(2) = point(2)-deltas*cos(theta);
 end
@@ -17,7 +17,7 @@ point=center;
 point(1) = point(1)-deltas*sin(theta);
 point(2) = point(2)-deltas*cos(theta);
 while point(1)<=dimx && point(2)<=dimy && point(1)>=1 && point(2)>=1
-    integral = integral + interp2(image,point(1),point(2));
+    integral = integral + interp2(image,point(1),point(2))*deltas;
     point(1) = point(1)-deltas*sin(theta);
     point(2) = point(2)+deltas*cos(theta);
 end
